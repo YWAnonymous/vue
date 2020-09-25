c05affac60203eb8f7be1690caeefbe4f4f9e500
YWAnonymous <1274315645@qq.com>
2020-09-25 13:50:55 +0800

    login

---
+++ src/views/Login/Login.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="login">
+    <div class="login-wrap">
+      <ul class="menu-tab">
+        <li v-for="item in menuTab" :class="{'current' : item.current}" :key="item.id">
+          {{ item.txt }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      menuTab: [
+        {
+          txt: "登录",
+          current: true,
+        },
+        {
+          txt: "注册",
+          current: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#login {
+  background-color: #344a5f;
+  height: 100vh;
+}
+.login-wrap {
+  width: 330px;
+  margin: auto;
+}
+.menu-tab {
+  text-align: center;
+  li {
+    display: inline-block; //这个是行级元素
+    width: 88px;
+    line-height: 36px;
+    font-size: 14px;
+    color: #fff;
+    border-radius: 2px;
+    cursor: pointer;
+  }
+  .current {
+    background-color: blue;
+  }
+}
+</style>
